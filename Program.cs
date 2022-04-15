385838e94c5334c995c5e47dabfe95dfa2f5b94f
Dogan Mus <doganmus87@gmail.com>
2022-04-15 19:36:26 +0300

    ilk soru çözüldü

--- Program.cs
+++ Program.cs
@@ -6,7 +6,23 @@ namespace PatikaOdev1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Ödev1 1.Soru
+            Console.Write("Pozitif bir sayı giriniz: ");
+            int n = int.Parse(Console.ReadLine());
+            
+            int[] ciftSayi = new int[n];
+            for (int i = 0 ; i < n; i++)
+            {
+               Console.Write("Pozitif sayı adedince sayı girişi yapınız: ");
+               int nSayi= int.Parse(Console.ReadLine());
+               if (nSayi%2==0)
+                Console.WriteLine(nSayi);
+               
+            }
+
+            //Ödev1 2.Soru
+            
+            
         }
     }
 }
