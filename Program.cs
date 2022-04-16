d95838bb9eaa3d793538c9b036f20309f8da487c
Dogan Mus <doganmus87@gmail.com>
2022-04-16 13:21:56 +0300

    Ödev1 2.Soru

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace PatikaOdev1
         static void Main(string[] args)
         {
             //Ödev1 1.Soru
-            Console.Write("Pozitif bir sayı giriniz: ");
+            /*Console.Write("Pozitif bir sayı giriniz: ");
             int n = int.Parse(Console.ReadLine());
             int[] diziSayi = new int[n];
             for (int i = 0 ; i < n; i++)
@@ -22,8 +22,24 @@ namespace PatikaOdev1
                {  
                   Console.WriteLine("Girilen çift sayı:" +diziSayi[i]);
                }
-            }
+            }*/
             //Ödev1 2.Soru
+            Console.WriteLine("Pozitif iki sayı giriniz :");
+            int n=int.Parse(Console.ReadLine()); 
+            int m=int.Parse(Console.ReadLine());
+            int[] dizi =new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(n +" pozitif tam sayıdan "+(i+1)+".sayı giriniz:");
+                dizi[i]=int.Parse(Console.ReadLine());  
+            }
+
+            foreach (int sayi in dizi)
+            {
+                if((sayi==m) ||(sayi%m==0))
+                    Console.WriteLine(sayi);
+            }
+            
             
             
         }
